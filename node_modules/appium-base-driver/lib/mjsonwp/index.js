3409dc1a4389a92f1227416de4c8f5095f842794
Matthew McIntyre <matthewtfm@gmail.com>
2017-06-13 19:29:12 -0400

    updating node_modules

--- node_modules/appium-base-driver/lib/mjsonwp/index.js
+++
@@ -1,9 +0,0 @@
-// transpile:main
-
-import { MJSONWP as MobileJsonWireProtocol, isSessionCommand,
-         routeConfiguringFunction } from './mjsonwp';
-import { NO_SESSION_ID_COMMANDS, ALL_COMMANDS } from './routes';
-import { errors, isErrorType, errorFromCode } from './errors';
-
-export { MobileJsonWireProtocol, routeConfiguringFunction, errors, isErrorType,
-         errorFromCode, ALL_COMMANDS, NO_SESSION_ID_COMMANDS, isSessionCommand };
--- node_modules/appium-xcuitest-driver/lib/logger.js
+++
@@ -1,5 +0,0 @@
-import { getLogger } from 'appium-logger';
-
-const log = getLogger('XCUITest');
-
-export default log;
